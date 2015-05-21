b4751b2f0590bfc8c665ff567e1997ea17740fbd
Dejan Kamenšek <kamensek@gmail.com>
2015-05-21 21:44:20 +0200

    Drop-down menu icon "click links"
    Leftclick on drop-down menu icon sends You to "My Activity Feed",
    rightclick to "Training/My Activities"

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,10 +1,11 @@
 var env = {
     preview: false, // Must be false in release
-    buildNumber: 603, // 0.6.0.3
-    analyticsTrackingID: 'UA-62527089-1', 	// my "Release" Google Analytics Code
-//    analyticsTrackingID: 'UA-62527089-2', 		// my "Development" Google Analytics Code
+    buildNumber: 604, // 0.6.0.4
+//    analyticsTrackingID: 'UA-62527089-1', 	// my "Release" Google Analytics Code
+    analyticsTrackingID: 'UA-62527089-2', 		// my "Development" Google Analytics Code
 
+    debugMode: true, // Must be false in release
+//    debugMode: false, // Must be false in release
     forceUpdated: false, // Must be false in release
-    debugMode: false, // Must be false in release
     useActivityStreamCache: true // Must be true in release
 };

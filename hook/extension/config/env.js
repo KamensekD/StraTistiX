590e14374675f6b243c955a910bb4c36c4f1e124
Dejan Kamenšek <kamensek@gmail.com>
2015-11-18 00:38:54 +0100

    release-2.0.0.1

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,11 +1,11 @@
 var env = {
     preview: false, // Must be false in release
-    buildNumber: 612, // 0.6.1.2
-//    analyticsTrackingID: 'UA-62527089-1', 	// my "Release" Google Analytics Code
-    analyticsTrackingID: 'UA-62527089-2', 		// my "Development" Google Analytics Code
+    buildNumber: 2001, // v2.0.0.1
+    analyticsTrackingID: 'UA-62527089-1', 	// my "Release" Google Analytics Code
+//    analyticsTrackingID: 'UA-62527089-2', 		// my "Development" Google Analytics Code
 
-    debugMode: true, // Must be false in release
-//    debugMode: false, // Must be false in release
+//    debugMode: true, // Must be false in release
+    debugMode: false, // Must be false in release
     forceUpdated: false, // Must be false in release
     useActivityStreamCache: true // Must be true in release
 };

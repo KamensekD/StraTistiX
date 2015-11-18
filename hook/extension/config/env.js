44c453481eaa72b7e8d5724ddfbec6328c41d4b2
Dejan Kamenšek <kamensek@gmail.com>
2015-11-18 09:59:53 +0100

    develop v2.0.0.2

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,11 +1,12 @@
 var env = {
     preview: false, // Must be false in release
-    buildNumber: 2001, // v2.0.0.1
-    analyticsTrackingID: 'UA-62527089-1', 	// my "Release" Google Analytics Code
-//    analyticsTrackingID: 'UA-62527089-2', 		// my "Development" Google Analytics Code
+    buildNumber: 2002, // v2.0.0.2 develop
+//    buildNumber: 2001, // v2.0.0.1
+//    analyticsTrackingID: 'UA-62527089-1', 	// my "Release" Google Analytics Code
+    analyticsTrackingID: 'UA-62527089-2', 		// my "Development" Google Analytics Code
 
-//    debugMode: true, // Must be false in release
-    debugMode: false, // Must be false in release
+    debugMode: true, // Must be false in release
+//    debugMode: false, // Must be false in release
     forceUpdated: false, // Must be false in release
     useActivityStreamCache: true // Must be true in release
 };

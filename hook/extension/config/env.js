3d7356b120748f70af89e3f7f6946d8e90938764
Dejan Kamenšek <kamensek@gmail.com>
2015-12-29 00:31:54 +0100

    release-2.2.2

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,12 +1,12 @@
 var env = {
-    buildNumber: 221, // v2.2.1 develop
+    buildNumber: 222, // v2.2.2 release
 //    based on ThomasChampagne's v2.2.1
     preview: false, // Must be false in release
-//    analyticsTrackingID: 'UA-62527089-1', 	// my "Release" Google Analytics Code
-    analyticsTrackingID: 'UA-62527089-2', 		// my "Development" Google Analytics Code
+    analyticsTrackingID: 'UA-62527089-1', 	// my "Release" Google Analytics Code
+//    analyticsTrackingID: 'UA-62527089-2', 		// my "Development" Google Analytics Code
 
-    debugMode: 2, // Debugmode/level - 0 or false=off, 1=some dbg.messages, 2=all dbg. messages   Must be false in release
-//    debugMode: false, // Must be false in release
+    debugMode: 0, // Debugmode/level - 0 or false=off, 1=some dbg.messages, 2=all dbg. messages   Must be false in release
+//    debugMode: false, // Must be false in release, also check Helper.js for helper debug!
     forceUpdated: false, // Must be false in release
     useActivityStreamCache: true // Must be true in release
 };
